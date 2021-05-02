2268f11738fda516324d1c83d3908d5465d6fa35
User <alejandro.ledezma.orellana@gmail.com>
2021-05-02 19:51:11 -0400

    Creamos el proyecto Practica4LayerArchitecture y los endpoints GRUD en su controlador

---
+++ Practica4LayerArchitecture/Practica4LayerArchitecture/Controllers/StudentsController.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Practica4LayerArchitecture.Controllers
+{
+    [ApiController]
+    [Route("[/api/students]")]
+    public class StudentsController : ControllerBase
+    {
+        public StudentsController()
+        {
+        }
+
+        [HttpPost]
+        public Student CreatePerson([FromBody] string studentName) // a. Create User
+        {
+            return new Student()
+            {
+                NombreDePila = studentName
+            };
+        }
+
+        [HttpPut]
+        public Student UpdateStudent([FromBody] Student student, List<Student> lista) // b. Update User
+        {
+            foreach (Student s in lista)
+                if (student == s)
+                    s.NombreDePila = "actualizado";
+
+            return student;
+        }
+
+        [HttpDelete]
+        public Student DeletePerson([FromBody] Student student, List<Student> lista) // c. Delete User
+        {
+            if (lista.Contains(student))
+                lista.Remove(student);
+
+            return student;
+        }
+
+        [HttpGet]
+        public List<Student> GetStudents() // d. Get User
+        {
+            List<Student> estudiantes = new List<Student>();
+            LlenarListaDeEstudiantes(estudiantes);
+            return estudiantes;
+        }
+        public void LlenarListaDeEstudiantes(List<Student> lista)
+        {
+            // metodo auxiliar para agregar estudiantes hardcodeados
+            Student a = new Student();
+            a.NombreDePila = "Frank"; a.Apellido1 = "Sinatra";
+            a.codigo = 1; a.carrera = "Derecho";
+            Student b = new Student();
+            b.NombreDePila = "Omar"; b.Apellido1 = "Banos";
+            b.codigo = 2; b.carrera = "Administracion de Empresas";
+            Student c = new Student();
+            c.NombreDePila = "George"; c.Apellido1 = "Kusonoki";
+            c.codigo = 3; c.carrera = "ISC";
+
+            lista.Add(a);
+            lista.Add(b);
+            lista.Add(c);
+        }
+    }
+}
---
+++ Practica4LayerArchitecture/Practica4LayerArchitecture/Student.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Practica4LayerArchitecture
+{
+    public class Student
+    {
+        public string NombreDePila { get; set; }
+        public string Apellido1 { get; set; }
+        public int codigo { get; set; }
+        public string carrera { get; set; }
+    }
+}
